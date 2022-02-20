0f64cb1877a61055da138a84c30eb011fab0a51f
ShonaliRomeshika <shonaliromeshika05@gmail.com>
2022-02-20 15:38:49 +0530

    4 frontend commit

--- hobbyology_frontend/src/utils/data.js
+++ hobbyology_frontend/src/utils/data.js
@@ -172,7 +172,7 @@ export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
         return query;
       };
 
-
+/*
       export const searchQuery = (searchTerm) => {
         const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
               image{
@@ -199,9 +199,34 @@ export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
         return query;
       };
 
-
+*/
       export const userSavedPinsQuery = (userId) => {
         const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+          image{
+            asset->{
+              url
+            }
+          },
+          _id,
+          destination,
+          postedBy->{
+            _id,
+            userName,
+            image
+          },
+          save[]{
+            postedBy->{
+              _id,
+              userName,
+              image
+            },
+          },
+        }`;
+        return query;
+      };
+
+      export const userCreatedPinsQuery = (userId) => {
+        const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
           image{
             asset->{
               url
